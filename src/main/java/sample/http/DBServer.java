4a43a292c9783327cabbc1d36066ec9f192a155f
sudharshan <sudharshan.cbe@gmail.com>
2013-05-17 11:14:57 +0530

    Update DBServer.java

--- src/main/java/sample/http/DBServer.java
+++ src/main/java/sample/http/DBServer.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package sample.http;
 import com.twitter.concurrent.ChannelSource;
 import com.twitter.finagle.Service;
